4df97e32abdf5790a83e6d0230e1ccfa9a2f8bee
Shivam Jaiswal <115680170+shivamjswlll@users.noreply.github.com>
2025-09-10 11:51:31 +0530

    Time: 0 ms (100%), Space: 21.6 MB (90.85%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,23 +1,27 @@
 class Solution {
-public:
-    bool isValid(int row,int col,vector<vector<char>> &board,char c){
-        for(int i=0;i<9;i++){
-            if(i != col && board[row][i] == c) return false;
-            if(i != row && board[i][col] == c) return false;
-            int duprow = 3*(row/3) + i/3;
-            int dupcol = 3*(col/3) + i%3;
-            if(duprow != row && dupcol != col && board[duprow][dupcol] == c) return false;
+    bool isValid(int row,int col,vector<vector<char>> &board){
+        char a = board[row][col];
+        for(int i = 0;i<9;i++){
+            if( i != col && board[row][i] == a) return false;
+            if( i != row && board[i][col] == a) return false;
+            int duprow = 3*(row/3)+ i/3;
+            int dupcol = 3*(col/3)+ i%3;
+            if(duprow != row && dupcol != col && board[duprow][dupcol] == a) return false;
         }
         return true;
     }
+    
+public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                if(board[i][j] != '.' && !isValid(i,j,board,board[i][j])) return false;
+        int m = board.size();
+        int n = board[0].size();
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(board[i][j] != '.'){
+                    if(!isValid(i,j,board)) return false;
+                }
             }
         }
-
         return true;
     }
 };
