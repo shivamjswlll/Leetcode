a958378f9c37e232067deb1f8ff8e0d234ebb079
Shivam Jaiswal <115680170+shivamjswlll@users.noreply.github.com>
2025-10-08 12:51:26 +0530

    Time: 34 ms (37.71%), Space: 26.9 MB (62.81%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,20 +1,22 @@
 class Solution {
-    private:
-    int solve(int i,int j,string &s,string &t,vector<vector<int>> &dp){
-        if(j>=t.size()) return 1;
-        if(i>=s.size()) return 0;
-
-    if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == t[j]){
-            return dp[i][j] = solve(i+1,j+1,s,t,dp) + solve(i+1,j,s,t,dp);
-        }
-        return dp[i][j] = solve(i+1,j,s,t,dp);
-    }
 public:
+    int solve(int i,int j,string &s,string &t,vector<vector<int>> &dp){
+        if(j >= t.size()) return 1;
+        if(i >= s.size()) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int take = 0;
+        if(s[i] == t[j]){
+            take = solve(i+1,j+1,s,t,dp);
+        }
+        int notTake = solve(i+1,j,s,t,dp);
+
+        return dp[i][j] = take + notTake;
+    }
     int numDistinct(string s, string t) {
-        int n1 = s.size();
-        int n2 = t.size();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
+        
+        vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
         return solve(0,0,s,t,dp);
     }
 };
