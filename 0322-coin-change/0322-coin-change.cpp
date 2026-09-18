53ae63fc6d3ce3de42cc49b930b31703152eacc5
Shivam Jaiswal <115680170+shivamjswlll@users.noreply.github.com>
2026-09-18 10:36:01 +0530

    Time: 58 ms (16.65%), Space: 49.9 MB (18.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,23 @@
 class Solution {
     int solve(int idx,int target,vector<int> &coins,vector<vector<int>> &dp){
-        if(idx >= coins.size() || target < 0) return 1e5;
-
-        if(dp[idx][target] != -1)  return dp[idx][target];
         if(target == 0){
             return 0;
         }
+        if(idx >= coins.size() || target < 0){
+            return 1e5;
+        }
+        if(dp[idx][target] != -1) return dp[idx][target];
 
         int take = 1 + solve(idx,target-coins[idx],coins,dp);
         int notTake = solve(idx+1,target,coins,dp);
 
-        return dp[idx][target] = min(take,notTake);
+        return dp[idx][target] =  min(take,notTake);
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        
         vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
         int ans = solve(0,amount,coins,dp);
 
-        return ans==1e5 ? -1:ans;
+        return (ans==1e5)?-1:ans;
     }
 };
